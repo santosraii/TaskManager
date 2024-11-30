38e897baf6e9b27c78562d1a643a4bdba4a29070
Raiane <raianes838@gmail.com>
2024-11-30 15:28:29 -0300

    Atualizando arquivos

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.getenv('DB_HOST', 'localhost')  # ou o IP correto, se necessário
+DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_NAME = os.getenv('DB_NAME', 'tarefas')
 DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASSWORD = os.getenv('DB_PASSWORD', '')
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')  # Se você tiver uma chave secreta definida no .env
+SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
